f9169e0b5b7aa4862bd460e41a91a32ba3467b32
pazvartanian <pazvartanian15@gmail.com>
2022-05-11 09:16:06 -0300

    odiciacion para cambio de Unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,16 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function cambiarUnidades (this.id, this.value) {
+    if (isNaN(valor)){
+        alert("Se agrego un valor invalido");
+        document.lasUnidades.unid_metro.value = "";
+        document.lasUnidades.unid_pulgada.value = "";
+        document.lasUnidades.unid_pie.value = "";
+        document.lasUnidades.unid_yarda.value = "";
+    }else if (id=="metro"){
+        document.lasUnidades.unid_pulgada.value = 39.3701*valor;
+    document.lasUnidades.unid_pie.value = 3.28084*valor;
+    document.lasUnidades.unid_yarda.value = 1.09361*valor;
+}
+}
